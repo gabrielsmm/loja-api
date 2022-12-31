1b35aa4ec76ea03d987c9360c64ec07a815da2b5
gabrielsmm <gabrielsmm251@gmail.com>
2022-12-31 16:16:30 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/gabrielsmm/loja/domain/Pagamento.java
+++ src/main/java/com/gabrielsmm/loja/domain/Pagamento.java
@@ -3,6 +3,7 @@ package com.gabrielsmm.loja.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.gabrielsmm.loja.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -23,6 +24,7 @@ public abstract class Pagamento implements Serializable {
 	private Integer id;
 	private Integer estado;
 	
+	@JsonBackReference
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId // Para ter o mesmo Id
---
+++ src/main/java/com/gabrielsmm/loja/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.gabrielsmm.loja.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielsmm.loja.domain.Pedido;
+import com.gabrielsmm.loja.repositories.PedidoRepository;
+import com.gabrielsmm.loja.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
