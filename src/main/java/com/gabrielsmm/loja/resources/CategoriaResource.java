8090b22413d627d67cb62d2002f14e234d262f2e
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-02 21:01:26 -0300

    Listando categorias com DTO

--- src/main/java/com/gabrielsmm/loja/resources/CategoriaResource.java
+++ src/main/java/com/gabrielsmm/loja/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.gabrielsmm.loja.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabrielsmm.loja.domain.Categoria;
+import com.gabrielsmm.loja.dto.CategoriaDTO;
 import com.gabrielsmm.loja.services.CategoriaService;
 
 @RestController
@@ -49,6 +52,12 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<CategoriaDTO> listDto = service.findAll().stream().map(CategoriaDTO::new).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 }
