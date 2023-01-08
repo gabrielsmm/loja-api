b56f840cad310a243bcaf8ca830cf5ca1982b5ce
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-07 21:48:05 -0300

    Inserção de Cliente com POST

--- src/main/java/com/gabrielsmm/loja/resources/ClienteResource.java
+++ src/main/java/com/gabrielsmm/loja/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.gabrielsmm.loja.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,14 +10,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabrielsmm.loja.domain.Cliente;
 import com.gabrielsmm.loja.dto.ClienteDTO;
+import com.gabrielsmm.loja.dto.ClienteNewDTO;
 import com.gabrielsmm.loja.services.ClienteService;
 
 import jakarta.validation.Valid;
@@ -32,6 +36,15 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping(value="/{id}")
