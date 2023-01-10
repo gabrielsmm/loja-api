b6896bd000246e471c3b66e053bd3c6be16c97a5
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-10 19:53:07 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/gabrielsmm/loja/repositories/ProdutoRepository.java
+++ src/main/java/com/gabrielsmm/loja/repositories/ProdutoRepository.java
@@ -1,9 +1,18 @@
 package com.gabrielsmm.loja.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gabrielsmm.loja.domain.Categoria;
 import com.gabrielsmm.loja.domain.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	
+	@Transactional(readOnly=true) // Apenas consulta, não é necessário criar uma transação
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
 }
