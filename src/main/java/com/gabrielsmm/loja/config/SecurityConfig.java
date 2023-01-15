b635b5b4e896c87be003e55529608a04cdf3950d
gabrielsmm <gabrielsmm251@gmail.com>
2023-01-15 16:49:20 -0300

    Autenticação e geração do token JWT pt.1

--- src/main/java/com/gabrielsmm/loja/config/SecurityConfig.java
+++ src/main/java/com/gabrielsmm/loja/config/SecurityConfig.java
@@ -3,12 +3,15 @@ package com.gabrielsmm.loja.config;
 // https://stackoverflow.com/questions/74680244/h2-database-console-not-opening-with-spring-security
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,6 +21,9 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
 
 //	private static final String[] PUBLIC_MATCHERS = {
 //			"/h2-console/**"
@@ -29,7 +35,8 @@ public class SecurityConfig {
 	};
 	
 	@Bean
-	SecurityFilterChain springWebFilterChain(HttpSecurity http) throws Exception {
+	SecurityFilterChain springWebFilterChain(HttpSecurity http, AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 		return http.cors().and().csrf().disable()
 				.headers().frameOptions().sameOrigin().and()
 				.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
