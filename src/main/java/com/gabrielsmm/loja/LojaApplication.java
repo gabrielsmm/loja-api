1de7b868e3e73d82f6245aeb28bd64a798e6c7a1
gabrielsmm <gabrielsmm251@gmail.com>
2023-02-02 20:46:21 -0300

    Enviando imagem via endpoint

--- src/main/java/com/gabrielsmm/loja/LojaApplication.java
+++ src/main/java/com/gabrielsmm/loja/LojaApplication.java
@@ -1,25 +1,13 @@
 package com.gabrielsmm.loja;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.gabrielsmm.loja.services.S3Service;
-
 @SpringBootApplication
-public class LojaApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
+public class LojaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-//		s3Service.uploadFile("D:\\temp\\mybrotha.jpg");
 	}
 
 }
--- src/main/java/com/gabrielsmm/loja/resources/ClienteResource.java
+++ src/main/java/com/gabrielsmm/loja/resources/ClienteResource.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gabrielsmm.loja.domain.Cliente;
@@ -79,6 +80,12 @@ public class ClienteResource {
 			@RequestParam(value="direction", defaultValue="ASC") String direction) {
 		Page<ClienteDTO> listDto = service.findPage(page, linesPerPage, orderBy, direction).map(ClienteDTO::new);
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping(value="/picture")
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
