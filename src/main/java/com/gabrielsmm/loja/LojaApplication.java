add5827b28f8babed016e712ae13167caeabdc71
gabrielsmm <gabrielsmm251@gmail.com>
2023-02-01 21:19:07 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/gabrielsmm/loja/LojaApplication.java
+++ src/main/java/com/gabrielsmm/loja/LojaApplication.java
@@ -1,13 +1,25 @@
 package com.gabrielsmm.loja;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gabrielsmm.loja.services.S3Service;
+
 @SpringBootApplication
-public class LojaApplication {
+public class LojaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		s3Service.uploadFile("D:\\temp\\mybrotha.jpg");
 	}
 
 }
